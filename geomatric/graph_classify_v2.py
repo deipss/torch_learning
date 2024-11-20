ffadd9ff8ceb0ace6b28bfbbb34c1884a446e4e1
deipss <deipss666@gmail.com>
2024-11-20 18:55:47 +0800

    generate graph

--- geomatric/graph_classify_v2.py
+++ geomatric/graph_classify_v2.py
@@ -50,6 +50,30 @@ args = parser.parse_args()
 
 #########################################################################
 
+def statistic_dataset(ds):
+    dataset = TUDataset(root=os.path.join(data_path, 'TUDataset'), name=ds)
+    print()
+    print(f'Dataset: {dataset}:')
+    print('====================')
+    print(f'Number of graphs: {len(dataset)}')
+    print(f'Number of features: {dataset.num_features}')
+    print(f'Number of classes: {dataset.num_classes}')
+
+    data = dataset[0]  # Get the first graph object.
+
+    print()
+    print(data)
+    print('=============================================================')
+
+    # Gather some statistics about the first graph.
+    print(f'Number of nodes: {data.num_nodes}')
+    print(f'Number of edges: {data.num_edges}')
+    print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
+    print(f'Has isolated nodes: {data.has_isolated_nodes()}')
+    print(f'Has self-loops: {data.has_self_loops()}')
+    print(f'Is undirected: {data.is_undirected()}')
+
+
 def load_data(start_index=0):
     dataset = TUDataset(root=os.path.join(data_path, 'TUDataset'), name=args.ds)
     dataset.to(device=_device)
@@ -151,6 +175,46 @@ class ResBlockGnn(torch.nn.Module):
         return y, global_mean
 
 
+class CrossBlockGnn(torch.nn.Module):
+    def __init__(self, hidden_channels, dataset, hidden_layer, model_name, res_graph=None):
+        super(CrossBlockGnn, self).__init__()
+        self.to_hidden_1 = get_block_model(model_name, dataset.num_node_features, hidden_channels)
+        self.to_hidden_2 = get_block_model(model_name, dataset.num_node_features, hidden_channels)
+        self.sequence = nn.Sequential()
+        for i in range(hidden_layer * 2):
+            self.sequence.add_module(f'{model_name}{i}',
+                                     get_block_model(model_name, hidden_channels,
+                                                     hidden_channels))
+        self.lin = nn.Linear(hidden_channels, dataset.num_classes)
+        self.res_graph = res_graph
+
+    def forward(self, x, edge_index, batch, graph_hidden=None):
+        # 1. Obtain node embeddings
+        x_cur_1 = F.relu(self.to_hidden_1(x, edge_index))
+        x_cur_2 = F.relu(self.to_hidden_2(x, edge_index))
+
+        x_pre_1 = torch.zeros(x_cur_1.shape, device=_device)
+        x_pre_2 = torch.zeros(x_cur_2.shape, device=_device)
+        i = 0
+        while i < len(self.sequence):
+            x_temp_1 = x_cur_1
+            x_temp_2 = x_cur_2
+            x_cur_1 = F.relu(self.sequence[i](x_cur_1 + x_pre_2, edge_index))
+            x_cur_2 = F.relu(self.sequence[i + 1](x_cur_2 + x_pre_1, edge_index))
+            x_pre_1 = x_temp_1
+            x_pre_2 = x_temp_2
+            i += 2
+        x_cur = x_cur_1 + x_cur_2
+        global_mean = global_mean_pool(x_cur, batch)  # [batch_size, hidden_channels]
+        if self.res_graph and graph_hidden is not None:  # use preview train result of graph embedding
+            global_mean = global_mean + graph_hidden
+        # 3. Apply a final classifier
+        y = F.dropout(global_mean, p=args.drop, training=self.training)
+        y = self.lin(y)
+
+        return y, global_mean
+
+
 class ResGraphBlockGnn(torch.nn.Module):
     def __init__(self, hidden_channels, dataset, hidden_layer, model_name):
         super(ResGraphBlockGnn, self).__init__()
@@ -191,10 +255,13 @@ def train_model(start_index):
     elif args.gname == 'GraphBlockGnn':
         model = GraphBlockGnn(hidden_channels=args.dim, dataset=dataset, hidden_layer=args.h_layer,
                               model_name=args.name)
+    elif args.gname == 'CrossBlockGnn':
+        model = CrossBlockGnn(hidden_channels=args.dim, dataset=dataset, hidden_layer=args.h_layer,
+                              model_name=args.name)
     else:
         print(f'no model name {args.gname}')
         return
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr,weight_decay=0.01)
     criterion = torch.nn.CrossEntropyLoss()
     model.to(device=_device)
     criterion.to(device=_device)
