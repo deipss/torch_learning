d85270cca6a7ed609731ebccf7c2ec4c1cbc1b7f
deipss <deipss666@gmail.com>
2025-04-15 10:29:00 +0800

    nlp

---
+++ nlp/audio_recognition.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    import whisper
+
+    model = whisper.load_model("medium")
+
+    # 指定语言（中文）
+    result = model.transcribe("/Users/deipss/Downloads/audio/人事处沟通.mp3", language="zh")
+    print("原始中文文本：", result["text"])
+    # 将中文音频翻译为英文文本
+    result_translated = model.transcribe("/Users/deipss/Downloads/audio/人事处沟通.mp3", task="translate", language="zh")
+
+
+    print("翻译为英文：", result_translated["text"])
